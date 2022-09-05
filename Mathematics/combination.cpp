5c5e1b36bb9e7c3977e2c3fb648b0b1e3e86404f
lee <lww7438@gmail.com>
2022-09-05 15:30:39 +0900

    Add combination logic

--- Mathematics/combination.cpp
+++ Mathematics/combination.cpp
@@ -1,12 +1,17 @@
 // Header Files
 #include <iostream>
 #include <string>
-#include <cstdlib>
 
 // Namespace & Aliases
 using std::string;
+using std::to_string;
 typedef string String;
+typedef long long Digit; // Digit Scale
 const char CHAR_OFFSET = '0';
+
+// Functions
+String get_binomial_coefficient(int n, int k);
+String add_str(String a, String b, size_t base = 10);
 
 /*
  *  Function Name : get_binomial_coefficient
@@ -32,7 +37,7 @@ String get_binomial_coefficient(int n, int k)
     // Calculation based on formula: nCk = n-1Ck-1 + n-1Ck
     for (int i = 2; i < n + 1; i++)
         for (int j = 1; j < i && j <= k; j++)
-            combTable[i][j] = combTable[i - 1][j - 1] + combTable[i - 1][j];
+            combTable[i][j] = add_str(combTable[i - 1][j - 1], combTable[i - 1][j]);
 
     // Set result (nCk)
     String result = combTable[n][k];
@@ -46,68 +51,51 @@ String get_binomial_coefficient(int n, int k)
     return result;
 }
 
-String operator+(String a, String b)
+String add_str(String a, String b, size_t base)
 {
     size_t size_a = a.size();
     size_t size_b = b.size();
-    size_t size_result = size_a > size_b ? size_a + 1 : size_b + 1; // size of result must be one larger than maximum size of operands (Because of the possibility of carry)
-    String result(size_result, '0');
 
-    unsigned short adder;
-    bool carry=false;
+    if (size_a == 0)
+        return b;
+    if (size_b == 0)
+        return a;
 
-    int i; // index
-
-    for (i = 0; i < size_a && i < size_b; i++)
-    {
-        adder = (a[i] - CHAR_OFFSET) + (b[i] - CHAR_OFFSET);
-
-        if (carry)
-            ++adder;
-
-        if (adder > 9)
-            carry = true;
-        else
-            carry = false;
-
-        if (carry)
-            result[i] = adder % 10 + CHAR_OFFSET;
-    }
-
-    if(size_a > size_b)
-    {
-        while(i<size_a)
-        {
-            result[i] = (a[i] - CHAR_OFFSET) + carry;
-            carry = false;
-            ++i;
-        }
-    }
-    else if(size_b > size_a)
-    {
-        while(i<size_b)
-        {
-            result[i] = (b[i] - CHAR_OFFSET) + carry;
-            carry = false;
-            ++i;
-        }
-    }
+    String big_num = size_a > size_b ? a : b;
+    String small_num = size_a > size_b ? b : a;
     
-    result[i] = carry+CHAR_OFFSET;
-    if(result[i] == '0')
-        result[i] = NULL;
-        
-    return result;
-}
+    String result = "";
 
-int main()
-{
-    int n, k;
-    while (true)
+    Digit idx_big = big_num.size() - 1;
+    Digit idx_small = small_num.size() - 1;
+    bool carry = 0;
+ 
+    while (idx_small > -1)
     {
-        std::cin >> n >> k;
-        if(n==-1)
-            break;
-        std::cout << get_binomial_coefficient(n, k) << '\n';
+        // int sum = atoi(&big_num[idx_big]) + atoi(&small_num[idx_small]) + carry;
+        int sum = (big_num[idx_big] - CHAR_OFFSET) + (small_num[idx_small] - CHAR_OFFSET) + carry;
+        carry = sum / base;
+        sum %= base;
+
+        result = to_string(sum) + result;
+
+        idx_big--;
+        idx_small--;
     }
+
+    while (idx_big > -1)
+    {
+        int sum = (big_num[idx_big] - CHAR_OFFSET) + carry;
+        carry = sum / base;
+        sum %= base;
+
+        result = to_string(sum) + result;
+
+        idx_big--;
+    }
+
+    if(carry)
+        result = "1" + result;
+
+    return result;
 }
